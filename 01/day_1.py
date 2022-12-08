0af1a57ca1977adb03512be81ca11e5250812869
Florence <89167809+Florencebelike@users.noreply.github.com>
2022-12-07 19:37:00 -0800

    Update day_1.py

--- 01/day_1.py
+++ 01/day_1.py
@@ -1,13 +1,8 @@
-f = open("day_1.txt", "r").read().splitlines()
+f = open("day_1.txt", "r").read().splitlines() #open file-->list
 
-n = [i for i, x in enumerate(f, 1) if x == '']
-elfs = [f[i:j]for i, j in zip([0]+ n, n)]
-cal = [sum(list(map(int, i[:-1]))) for i in elfs]
+n = [i for i, x in enumerate(f, 1) if x == ''] # find all space
+elfs = [f[i:j]for i, j in zip([0]+ n, n)] #list--> sublists
+cal = [sum(list(map(int, i[:-1]))) for i in elfs] #list of sum of each
 
-#max
-print(cal.index(max(cal)), max(cal))
-
-#top three sum
-print(sum(sorted(cal)[-3:]))
-
-        
+print(cal.index(max(cal)), max(cal)) #max
+print(sum(sorted(cal)[-3:])) #top three sum
